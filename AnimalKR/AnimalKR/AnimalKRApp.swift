8eb227dd40b1830fb94dc2ed2067e9f8d491c055
cshyeon32 <139736071+cshyeon32@users.noreply.github.com>
2024-10-15 20:02:36 +0900

    create

---
+++ AnimalKR/AnimalKR/AnimalKRApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalKRApp.swift
+//  AnimalKR
+//
+//  Created by dgsw8th07 on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimalKRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimalKR/AnimalKR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimalKR
+//
+//  Created by dgsw8th07 on 10/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
